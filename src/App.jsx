226d1d2fbdc8b9e0d7672ddb98ff73e2a4f7eb58
satnam-1203 <167192846+satnam-1203@users.noreply.github.com>
2024-04-20 21:32:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
